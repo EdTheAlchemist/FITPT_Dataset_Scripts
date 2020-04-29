f4af6956b7528d14f473ce3ca27dcf5c38996a26
Ed <edward.tighe@dlsu.edu.ph>
2020-04-29 19:56:28 +0800

    Raw Personality Score Processing Updates + Documentation
    This update is mainly for extracting information from the raw personality scores. Key features involve:
    1. Computing the correlation of each personality trait with each other (and saving to file)
    2. Fixing the string that holds answers to each personality test item (and saving to file
    3. Computing the Cronbach's Alpha for each trait (and saving to file).
    
    Internal documentation was also made in both the personality script and the filter user script.
    
    Minor fix in name of filter user script.

--- CleanAndFilterUsers.py
+++ CleanAndFilterUsers.py
@@ -5,7 +5,7 @@ sys.path.insert(0, './Constants')
 
 ##### MAIN IMPORTS #####
 
-from Paths import USER_FILE_PATH, PERSONALITY_FILE_PATH, INSTA_PROFILE_FILE_PATH, TWITTER_PROFILE_FILE_PATH, INSTA_POST_FILE_PATH, TWITTTER_POST_FILE_PATH, GENERATED_DATA_FOLDER_PATH
+from Paths import USER_FILE_PATH, PERSONALITY_FILE_PATH, INSTA_PROFILE_FILE_PATH, TWITTER_PROFILE_FILE_PATH, INSTA_POST_FILE_PATH, TWITTTER_POST_FILE_PATH, FILTERED_USER_FILE_PATH
 from CollectionDates import COLLECTION_DATES
 
 from datetime import datetime
@@ -40,30 +40,37 @@ def calculate_age(text):
 
 ##### START OF SCRIPT #####
 
-users_to_remove = []
-
+# Load in CSV for all users (raw data)
 user_df = pd.read_csv(USER_FILE_PATH, encoding='utf-8')
 user_df.set_index("UserID", inplace=True)
 user_df['Age'] = user_df['Birthdate'].apply(calculate_age)
 user_df = user_df[['Sex', 'Nationality', 'Age']]
 
+# Load in CSV of personality scores (raw data)
 personality_df = pd.read_csv(PERSONALITY_FILE_PATH, encoding='utf-8')
 personality_df.set_index("UserID", inplace=True)
 
+# Identify users to removee (filters)
+# (1) Age less than 18, (2) Nationality doesn't contain "Filipino", (3) Errors with personality scores
+users_to_remove = []
 users_to_remove += user_df.Age[user_df.Age < 18].index.tolist()
 users_to_remove += user_df.Nationality[~user_df.Nationality.str.contains("Filipino")].index.tolist()
 users_to_remove += personality_df.Answers[personality_df.Answers.str.contains("undefined")].index.tolist()
 user_df = user_df[~user_df.index.isin(users_to_remove)]
 
+# Load in Twitter account data
 twitter_df = pd.read_csv(TWITTER_PROFILE_FILE_PATH, encoding='utf-8')
 twitter_df.set_index("UserID", inplace=True)
 
+# Load in Instagram account data
 instagram_df = pd.read_csv(INSTA_PROFILE_FILE_PATH, encoding='utf-8')
 instagram_df.set_index("UserID", inplace=True)
 
+# Plot whether a participant has a Twitter and/or Instagram account
 user_df["HasTwitter"] = user_df.index.isin(twitter_df.index)
 user_df["HasInstagram"] = user_df.index.isin(instagram_df.index)
 for trait in TRAITS:
 	user_df[trait] = personality_df[trait]
-	
-user_df.to_csv(GENERATED_DATA_FOLDER_PATH + "filtered_users.csv", encoding='utf-8')
+
+# Save final file to CSV
+user_df.to_csv(FILTERED_USER_FILE_PATH, encoding='utf-8')
---
+++ FilterPersonalityTestAnswers.py
@@ -0,0 +1,93 @@
+from __future__ import print_function, division
+
+import sys
+sys.path.insert(0, './Constants')
+
+##### MAIN IMPORTS #####
+
+from Paths import PERSONALITY_FILE_PATH, FILTERED_USER_FILE_PATH, PERSONALITY_COOR_FILE_PATH, PERSONALITY_ITEM_SCORES_FILE_PATH, PERSONALITY_CRONBACHS_ALPHA_FILE_PATH
+from StringIO import StringIO
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+##### CONSTANTS #####
+
+REVERSE_SCORING = {
+	'1': 5,
+	'2': 4,
+	'3': 3,
+	'4': 2,
+	'5': 1
+}
+SCORES_TO_REVERSE = [6, 21, 31, 2, 12, 27, 37, 8, 18, 23, 43, 9, 24, 34, 35, 41]
+ITEM_TO_TRAIT = {
+	'Extraversion': [1, 6, 11, 16, 21, 26, 31, 36],
+	'Agreeableness': [2, 7, 12, 17, 22, 27, 32, 37, 42],
+	'Conscientiousness': [3, 8, 13, 18, 23, 28, 33, 38, 43],
+	'Neuroticism': [4, 9, 14, 19, 24, 29, 34, 39],
+	'Openness': [5, 10, 15, 20, 25, 30, 35, 40, 41, 44],
+}
+##### FUNCTIONS #####
+
+def reverse_score(number):
+	return REVERSE_SCORING[str(number)]
+
+# Taken from https://github.com/anthropedia/tci-stats/blob/master/tcistats/__init__.py
+# Modified -- input is already of dataframe type
+def cronbach_alpha(items):
+    items_count = items.shape[1]
+    variance_sum = float(items.var(axis=0, ddof=1).sum())
+    total_var = float(items.sum(axis=1).var(ddof=1))
+    
+    return (items_count / float(items_count - 1) * (1 - variance_sum / total_var))
+
+##### START OF SCRIPT #####
+
+# Load in personality data
+personality_df = pd.read_csv(PERSONALITY_FILE_PATH, encoding="utf-8")
+personality_df.set_index("UserID", inplace=True)
+
+# Load in filtered user data
+filtered_user_df = pd.read_csv(FILTERED_USER_FILE_PATH, encoding='utf-8')
+filtered_user_df.set_index("UserID", inplace=True)
+
+# Remove / filter out participants who don't qualify / Retain those who do
+personality_df = personality_df[personality_df.index.isin(filtered_user_df.index)]
+
+# Get correlation between each personality trait
+personality_df[[x for x in ITEM_TO_TRAIT]].corr().to_csv(PERSONALITY_COOR_FILE_PATH)
+
+# Clean / fix individual answers to each question
+# Focus on answer string
+personality_df = personality_df['Answers']
+
+# Split string and convert to number type
+personality_df = personality_df.str.split("-", expand=True)
+personality_df = personality_df.astype(int)
+
+# Properly name headers as "item_x", where x is a number from 1 to 44 
+# Numbers correspond to each of the 44 items in the BFI
+rename_dict = {}
+for num in range(1,45):
+	rename_dict[num-1] = "item_%d" % num
+personality_df.rename(columns=rename_dict, inplace=True)
+
+# Process reverse scores (e.g. 5 --> 1, 2 --> 4)
+# See BFI-44 manual for more details
+for score in SCORES_TO_REVERSE:
+	key = "item_%d" % (score)
+	personality_df[key] = personality_df[key].apply(reverse_score)
+
+# Save individual answers to a CSV file
+personality_df.to_csv(PERSONALITY_ITEM_SCORES_FILE_PATH, encoding='utf-8')
+
+# Generate Cronbach's Alpha for each dimension
+alpha_coefs = []
+for trait in ITEM_TO_TRAIT:
+	trait_df = personality_df[["item_%s" % x for x in ITEM_TO_TRAIT[trait]]]
+
+	alpha_coefs.append([trait, cronbach_alpha(trait_df)])
+
+# Generate DataFrame of alpha coefficients and save to CSV
+pd.DataFrame(alpha_coefs, columns=["Trait", "Cronbach's Alpha"]).to_csv(PERSONALITY_CRONBACHS_ALPHA_FILE_PATH)
