a51a0b3cc234aa80930a278ab74717473accb7ad
Ed <edward.tighe@dlsu.edu.ph>
2020-05-14 00:34:10 +0800

    Update FilterAndFixTextPosts.py
    Addressed issue with line breaks screwing up the csv reading process. As the solution, quoting for all values will now take place to ensure values are properly identified.
    
    Also, added line 34, which removes a column that will never be used in processing (since images were already downloaded)

--- FilterAndFixTextPosts.py
+++ FilterAndFixTextPosts.py
@@ -10,6 +10,7 @@ from Paths import INSTA_POST_FILE_PATH, TWITTTER_POST_FILE_PATH
 from ftfy import fix_encoding
 
 import pandas as pd
+import csv
 
 ##### START OF SCRIPT #####
 
@@ -22,12 +23,15 @@ twita_user_lists = user_df[user_df.HasTwitter].index.tolist()
 insta_user_lists = user_df[user_df.HasInstagram].index.tolist()
 
 # Load in CSV files
-twita_text_df = pd.read_csv(TWITTTER_POST_FILE_PATH, encoding="utf-8")
+twita_text_df = pd.read_csv(TWITTTER_POST_FILE_PATH, encoding="utf-8", sep=',')
 twita_text_df.set_index("TweetID", inplace=True)
 twita_text_df['Text'].fillna("", inplace=True)
-insta_text_df = pd.read_csv(INSTA_POST_FILE_PATH, encoding="utf-8")
+insta_text_df = pd.read_csv(INSTA_POST_FILE_PATH, encoding="utf-8", sep=',')
 insta_text_df.set_index("InstaPostID", inplace=True)
 insta_text_df['Caption'].fillna("", inplace=True)
+
+# For Instagram posts, remove URL column since that won't be needed moving forward
+insta_text_df = insta_text_df[[u'UserID',u'DatePosted', u'Caption', u'LikesCount', u'Location']]
 
 # Remove posts that belong to users that didn't qualify for participation
 twita_text_df = twita_text_df[twita_text_df['UserID'].isin(twita_user_lists)]
@@ -38,5 +42,6 @@ twita_text_df['Text'] = twita_text_df['Text'].astype(unicode).apply(fix_encoding
 insta_text_df['Caption'] = insta_text_df['Caption'].astype(unicode).apply(fix_encoding)
 
 # Save fixed files to input folders (there is no need to keep data that's from non-qualifying participants)
-twita_text_df.to_csv(TWITTTER_POST_FILE_PATH, encoding="utf-8")
-insta_text_df.to_csv(INSTA_POST_FILE_PATH, encoding="utf-8")
+# Force all values to be quoted because of possible issues with line breaks
+twita_text_df.to_csv(TWITTTER_POST_FILE_PATH, encoding="utf-8", sep=',', quoting=csv.QUOTE_ALL)
+insta_text_df.to_csv(INSTA_POST_FILE_PATH, encoding="utf-8", sep=',', quoting=csv.QUOTE_ALL)
